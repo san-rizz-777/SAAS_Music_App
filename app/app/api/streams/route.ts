3dbef1755c190d79f4f080d9924ae84fa07a082b
Sanskar Gunde <gundesanskar71@gmail.com>
2026-05-19 22:34:26 +0530

    Create post stream end-point.

--- app/app/api/streams/route.ts
+++ app/app/api/streams/route.ts
@@ -1,5 +1,8 @@
 import {NextRequest, NextResponse} from "next/server";
 import  {z} from "zod";
+import {prismaClient} from "@/app/lib/db";
+
+const YT_REG = new RegExp("^https:\/\/www\.com\/watch\?v=[\w-]{11}$");
 
 //Creator schema
 const CreatorStreamSchema = z.object({
@@ -12,6 +15,30 @@ const CreatorStreamSchema = z.object({
 export async function POST(req: NextRequest) {
     try {
         const data = CreatorStreamSchema.parse(await req.json());
+        const isYt = YT_REG.test(data.url); //validate for regex
+        if(!isYt)
+        {
+            //give appropriate info for regex error
+            return NextResponse.json({
+                message:"Wrong URL found!!!"
+            }, {
+                status: 411
+            })
+        }
+
+        //extract the id
+        const extractedId = data.url.split("?v=")[1];
+
+        //create the stream
+        // @ts-ignore
+        await prismaClient.stream.create({
+            data:{
+                userId: data.creatorId,
+                url: data.url,
+                extractedId,
+                type: "Youtube"
+            }
+        })
     }
     catch(e){
         return NextResponse.json({
