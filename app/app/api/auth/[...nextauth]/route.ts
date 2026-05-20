286364e81dc5a7507dcf4e4f98b7bc8b962acc00
Sanskar Gunde <gundesanskar71@gmail.com>
2026-05-20 18:20:07 +0530

    Add signIn callback.

--- app/app/api/auth/[...nextauth]/route.ts
+++ app/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import {prismaClient} from "@/app/lib/db";
 
 const handler = NextAuth({
         providers : [
@@ -7,7 +8,28 @@ GoogleProvider({
     clientId : process.env.GOOGLE_CLIENT_ID ?? "",
     clientSecret : process.env.GOOGLE_CLIENT_SECRET ?? "",
 })
-        ]
+        ],
+    callbacks: {
+            async signIn(params){
+                console.log(params);
+                //error handling
+                try{
+                    await prismaClient.user.create({
+                        data: {
+                            email: "",
+                            provider: "Google",
+                            role: "Streamer"
+                        }
+                    })
+                }
+                catch(e)
+                {
+                    console.log(e);
+                }
+
+                return true;
+            }
+    }
 })
 
 export {handler as GET, handler as POST}
