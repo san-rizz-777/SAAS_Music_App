0684783e64d7cdf016de6ff26cce80102529115b
Sanskar Gunde <gundesanskar71@gmail.com>
2026-06-10 09:02:34 +0530

    Ignore the type of youtube-search-api.

--- app/app/api/streams/route.ts
+++ app/app/api/streams/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "@/lib/db";
+//@ts-ignore
 import youtubesearchapi from "youtube-search-api";
 import { getServerSession } from "next-auth";
 import {authOptions} from "@/lib/next-options";
